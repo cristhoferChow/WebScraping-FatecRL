02274be85599a97cfb831e033fde1b1c1546d67c
cristhoferChow <74063033+cristhoferChow@users.noreply.github.com>
2024-04-10 09:57:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function pegarCod() {
     document.querySelectorAll(".tab-grid-content-header.tab-text").forEach(div => {
         let [, cod] = div.textContent.trim().split("|");
-        if (cod) console.log(cod.trim());
+        if (cod) 
+        console.log(cod.trim());
     });
 }
